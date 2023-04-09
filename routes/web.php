8951d011651e7964ee08a6935c40ae4530a6133e
Konnstantin Naumov <naumov@MacBook-Pro-Konnstantin.local>
2023-04-09 22:01:09 +0300

    CHAT-1: Docs page

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('/docs')
+    ->namespace('App\Http\Controllers\Web')
+    ->group(function () {
+        Route::get('/', 'DocsController@form');
+        Route::get('/swagger-file', 'DocsController@swaggerFile');
+    });
