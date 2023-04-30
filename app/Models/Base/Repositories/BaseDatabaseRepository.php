b587ede36945561406984a353335d33e474bb99a
Konnstantin Naumov <naumov@MacBook-Pro-Konnstantin.local>
2023-04-30 21:28:15 +0300

    CHAT-1. Test

--- app/Models/Base/Repositories/BaseDatabaseRepository.php
+++ app/Models/Base/Repositories/BaseDatabaseRepository.php
@@ -2,9 +2,8 @@
 
 namespace App\Models\Base\Repositories;
 
-use App\Models\User\Model;
+use App\Enums\EnvTypesEnum;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\DB;
 
 /**
  * Class BaseDatabaseRepository
@@ -25,6 +24,12 @@ abstract class BaseDatabaseRepository
     protected const FOLLOWER_CONNECTION_NAME = 'database_follower';
 
     /**
+     * Testing database connection name
+     * @var string
+     */
+    protected const TESTING_CONNECTION_NAME = 'database_testing';
+
+    /**
      * Get model class name
      *
      * @return string
@@ -39,6 +44,16 @@ abstract class BaseDatabaseRepository
     abstract protected function getTableName(): string;
 
     /**
+     * Check is current env testing
+     *
+     * @return bool
+     */
+    protected function isTesting(): bool
+    {
+        return config('app.env') === EnvTypesEnum::TESTING;
+    }
+
+    /**
      * Get query builder instance to leader database
      *
      * @return Builder
@@ -49,7 +64,7 @@ abstract class BaseDatabaseRepository
         /**
          * @var Builder $query
          */
-        $query = $className::on(self::LEADER_CONNECTION_NAME);
+        $query = $className::on($this->isTesting() ? self::TESTING_CONNECTION_NAME : self::LEADER_CONNECTION_NAME);
 
         return $query;
     }
@@ -65,7 +80,7 @@ abstract class BaseDatabaseRepository
         /**
          * @var Builder $query
          */
-        $query = $className::on(self::FOLLOWER_CONNECTION_NAME);
+        $query = $className::on($this->isTesting() ? self::TESTING_CONNECTION_NAME : self::FOLLOWER_CONNECTION_NAME);
 
         return $query;
     }
--- tests/UseCase/User/RegisterRandomUserUseCaseTest.php
+++ tests/UseCase/User/RegisterRandomUserUseCaseTest.php
@@ -1,8 +1,45 @@
 <?php
 
-namespace UseCase\User;
+namespace Tests\UseCase\User;
 
-class RegisterRandomUserUseCaseTest
+use App\Enums\UseCaseSystemNamesEnum;
+use App\Models\User\Model;
+use App\UseCases\Base\Exceptions\UseCaseNotFoundException;
+use App\UseCases\User\RegisterRandomUserUseCase;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Tests\UseCase\BaseUseCaseTest;
+
+/**
+ * Class RegisterRandomUserUseCaseTest
+ * @package Tests\UseCase\User
+ */
+final class RegisterRandomUserUseCaseTest extends BaseUseCaseTest
 {
+    /**
+     * Test success case
+     *
+     * @test
+     * @return void
+     * @throws UseCaseNotFoundException
+     * @throws BindingResolutionException
+     */
+    public function testSuccessCase(): void
+    {
+        /**
+         * @var RegisterRandomUserUseCase $useCase
+         */
+        $useCase = $this->useCaseFactory->createUseCase(
+            UseCaseSystemNamesEnum::REGISTER_RANDOM_USER
+        );
+        $useCase->execute();
 
+        $outputDto = $useCase->getOutputDto();
+
+        $this->assertNotNull($outputDto->token);
+        $this->assertNotNull($outputDto->username);
+
+        $this->assertNotNull(
+            Model::query()->where('username', $outputDto->username)->first()
+        );
+    }
 }
