d07e655ccc08c565bd6a7e39f74ee5fd8bd5c57b
Konnstantin Naumov <naumov@MacBook-Pro-Konnstantin.local>
2023-08-30 22:22:23 +0300

    CHAT-11: Get messages. Docs

---
+++ app/Http/Requests/Api/V1/Message/IndexRequest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\Message;
+
+class IndexRequest
+{
+
+}
---
+++ app/Models/Message/Repositories/CacheRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models\Message\Repositories;
+
+class CacheRepository
+{
+
+}
